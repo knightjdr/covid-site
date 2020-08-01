faec91be515fdaf9d9b4344897ecb0cb5bf09e48
James Knight <knightjdr@gmail.com>
2020-08-01 10:39:41 -0400

    Fix text

--- src/components/display/network/network-legend.js
+++ src/components/display/network/network-legend.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NetworkLegend = () => (
   <p className="display__legend network__legend">
-    Network view of the viral proteins with their (at most) top twenty-five preys. Baits
-    are in
+    Network view of viral proteins with their (at most) top twenty-five proximity interactions.
+    Viral proteins are in
     {' '}
     <span className="network__legend_purple">purple</span>
     {' '}
