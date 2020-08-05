b61153b0fab6ad6c199393ccd5cd37ca8fb120c7
James Knight <knightjdr@gmail.com>
2020-08-05 14:32:30 -0400

    Add interactome infographic

---
+++ src/components/display/interactome/interactome-grid.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import HumanCell from './assets/human-cell.svg';
+import InfectedCell from './assets/infected-cell.svg';
+import Virus from './assets/virus.svg';
+
+const InteractomeGrid = () => (
+  <div className="interactome__grid">
+    <div className="interactome__grid-item interactome__human-proteome">
+      <h3>Human proteome &#8776; 10K proteins</h3>
+      <div className="interactome__grid-item-content">
+        <img
+          alt="Human cell"
+          className="interactome__grid-item-image"
+          height={120}
+          src={HumanCell}
+          width={120}
+        />
+        <p>
+          The A549 proteome has not been precisely defined, but typical cellular
+          proteomes consit of approximately ten-thousand proteins.
+        </p>
+      </div>
+    </div>
+    <div className="interactome__grid-item interactome__viral-proteome">
+      <h3>Viral proteome = 29 proteins</h3>
+      <div className="interactome__grid-item-content">
+        <img
+          alt="Virus"
+          className="interactome__grid-item-image"
+          height={120}
+          src={Virus}
+          width={120}
+        />
+        <p>
+          The SARS-Cov-2 genome encodes twenty-nine known proteins, twenty-seven of which
+          were profiled in this study.
+        </p>
+      </div>
+    </div>
+    <div className="interactome__grid-item interactome__proximity-interactome">
+      <h3>Proximity interactome = 2 242 proteins </h3>
+      <div className="interactome__grid-item-content">
+        <img
+          alt="Cell infected with SARS-CoV-2"
+          className="interactome__grid-item-image"
+          height={120}
+          src={InfectedCell}
+          width={120}
+        />
+        <p>
+          We detected over 20% of the A549 proteome to be in proximity to
+          at least one of the viral proteins we expressed. The N protein
+          had only six proximity interactions while M had 938 or ~10% of the cellular proteome.
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default InteractomeGrid;
--- src/components/display/interactome/interactome.js
+++ src/components/display/interactome/interactome.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAbacus } from '@fortawesome/pro-duotone-svg-icons';
 
+import InteractomeGrid from './interactome-grid';
 import Link from '../../link/link';
+
+import './interactome.css';
 
 const Interactome = () => (
   <section className="display">
     <div className="display__inner">
       <h2
         className="display__title"
-        id="heatmap"
+        id="interactome"
       >
         <FontAwesomeIcon icon={faAbacus} />
         Interactome
       </h2>
       <p className="display__legend">
-        We detected 11 200 total proximity interactions between exogenous viral baits and
+        We detected 7 810 total proximity interactions between exogenous viral baits and
         native human preys. These interactions are amongst 2 242 unique human proteins.
         {' '}
         <Link
@@ -25,7 +28,7 @@ const Interactome = () => (
           NSP12
         </Link>
         {' '}
-        had the fewest (6) and
+        had the fewest number of interactions (6) and
         {' '}
         <Link
           nav
@@ -39,6 +42,7 @@ const Interactome = () => (
         <Link nav to="/heatmap">heat map</Link>
         .
       </p>
+      <InteractomeGrid />
     </div>
   </section>
 );
